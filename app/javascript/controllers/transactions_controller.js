82fa591dec359dd0ae8ef55b7fa7ea0f8a775788
angus mcdonald <angusmcdonald13@gmail.com>
2022-09-15 10:50:47 +1000

    fixing

--- app/javascript/controllers/transactions_controller.js
+++ app/javascript/controllers/transactions_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="transactions"
+//  Connects to data-controller="transactions"
 export default class extends Controller {
   static targets = ["form"]
 
