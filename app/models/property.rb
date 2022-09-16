da879270ac43bc6861e8c720ec988df91fea9ca2
angus mcdonald <angusmcdonald13@gmail.com>
2022-09-16 12:07:07 +1000

    mapbox show page

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,8 @@ class Property < ApplicationRecord
   belongs_to :user
   has_many :transactions
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   def total_profit
     transactions.map do |transaction|
