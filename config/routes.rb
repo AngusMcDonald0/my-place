7683ec6fb4ab7bdbf0164f04cf8d7f24c6e36f99
angus mcdonald <angusmcdonald13@gmail.com>
2022-09-20 18:22:19 +1000

    added destroy, halfway there with edit function for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :properties, only: %i[index show new create] do
-    resources :transactions, only: :create
+    resources :transactions, only: %i[create update edit]
   end
+  resources :transactions, only: :destroy
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
